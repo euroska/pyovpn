aa43b51fafd6a5abb46835efed1f0ea5e0d3c4ce
Martin Miksanik <martin@miksanik.net>
2018-03-30 21:47:22 +0200

    Cleaning manifest and add index to root

--- setup.py
+++ setup.py
@@ -1,17 +1,22 @@
 # -*- coding: utf-8 -*-
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='pyovpn',
     version='1.0',
-    description='Config for OpenVPN',
-    author='Martin Miksanik',
-    author_email='miksanik@gmail.com',
+    description='OpenVPN orchestrate util',
+    author='Martin Miksanik, Jiri Peterek',
+    author_email='miksanik@gmail.com, jiri.peterek@gmail.com',
     url='https://github.com/euroska/pyovpn',
-    packages=['pyovpn'],
+    packages=find_packages('pyovpn'),
+    include_package_data=True,
     install_requires=[
-        #todo add dependencies
         'aiohttp',
+        'yaml',
+        'jsonschema',
+        'jinja2',
+        'cryptography',
+        'pytz'
     ],
     scripts=[
         'bin/pyovpn'
